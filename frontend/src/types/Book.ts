2926a20e6c7545ee3dac2e9d7b23e11db6d57667
nathanctay <63444651+nathanctay@users.noreply.github.com>
2026-04-16 17:47:30 -0600

    adjust Book type to align closer with hardcover api

--- frontend/src/types/Book.ts
+++ frontend/src/types/Book.ts
@@ -1,8 +1,10 @@
 export interface Book {
     id: number | string;
+    hardcoverId: number;
     title: string;
     pageCount: number;
     author: string;
+    coverUrl: string;
     progress?: number;
     complete?: boolean;
     date_started?: Date;
