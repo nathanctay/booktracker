2fb7af9be0fcc753c8979ffd04b666fcd725b1a4
nathanctay <63444651+nathanctay@users.noreply.github.com>
2026-04-16 17:47:03 -0600

    search page that actually gets results

--- frontend/src/pages/BookInfoPage.tsx
+++ frontend/src/pages/BookInfoPage.tsx
@@ -1,4 +1,24 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router"
+import { getBookInfo } from "../utils/books"
+
 function BookInfoPage() {
+    const params = useParams()
+    const bookId = params.id
+    const [bookInfo, setBookInfo] = useState({})
+
+    useEffect(() => {
+        async function getInfo() {
+            if (bookId) {
+                const results = await getBookInfo(bookId)
+                setBookInfo(results)
+                console.log(results)
+            }
+        }
+
+        getInfo()
+    }, [params])
+
     return (
         <div>BookInfoPage</div>
     )
--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router"
-import { getBookInfo, getBooks } from "../utils/Books";
+import { Link, useSearchParams } from "react-router"
+import { getBookInfo, getBooks } from "../utils/books";
 
 function SearchPage() {
     const [searchParams, setSearchParams] = useSearchParams();
@@ -20,17 +20,20 @@ function SearchPage() {
     }, [search])
 
     useEffect(() => {
-        if (searchParams.get('q') != '') {
-            console.log(searchParams.toString())
-            // getBookResults()
+        async function getBookResults() {
+            const q = searchParams.get('q')
+            if (q) {
+                const bookResults = await getBooks(q)
+                setBooks(bookResults)
+            }
+        }
+
+        if (searchParams.get('q') && searchParams.get('q') != '') {
+            getBookResults()
         }
     }, [searchParams])
 
-    async function getBookResults() {
-        console.log(searchParams.toString())
-        let books = await getBooks(searchParams.toString())
-        setBooks(books)
-    }
+
 
 
 
@@ -39,21 +42,23 @@ function SearchPage() {
             <div>SearchPage</div>
             <input value={search} type="text" className="border rounded-sm w-full px-4 py-2 outline-[#ff0000]" onChange={(event) => setSearch(event.target.value)} />
             {
-                books.length > 0 && (
+                books && books.length > 0 && (
                     <ul className="flex flex-col gap-2 mt-4">
                         {books.map((book, index) => {
                             return (
-                                <li onClick={() => console.log(book)}>
-                                    <div className="flex border rounded-md pl-2 pr-4 py-2 gap-2">
-                                        <div>
-                                            <img src={`https://covers.openlibrary.org/b/id/${book.cover_i}-S.jpg`} alt={`${book.title} cover`} />
+                                <Link to={`/book/${book.id}`}>
+                                    <li onClick={() => console.log(book)}>
+                                        <div className="flex border rounded-md pl-2 pr-4 py-2 gap-2">
+                                            <div>
+                                                <img width={20} src={book.coverUrl} alt={`${book.title} cover`} />
+                                            </div>
+                                            <div className="flex-4">
+                                                <span className='font-bold'>{book.title}</span> <span className='text-sm'>{book.author && `by ${book.author}`}</span>
+                                            </div>
+                                            <div className='flex-1'>{book.pageCount} <span className='small'>pages</span></div>
                                         </div>
-                                        <div className="flex-4">
-                                            <span className='font-bold'>{book.title}</span> <span className='text-sm'>{book.author && `by ${book.author}`}</span>
-                                        </div>
-                                        <div className='flex-1'>{book.pageCount} <span className='small'>pages</span></div>
-                                    </div>
-                                </li >
+                                    </li >
+                                </Link>
                             )
                         })}
                     </ul>
